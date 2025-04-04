548ab29865900fc8386ddaf4b73255da0a6ca85a
kausik-m <65801695+kausik-m@users.noreply.github.com>
2025-04-04 23:19:04 +0530

    added hello kausik

--- hello_world.c
+++ hello_world.c
@@ -3,7 +3,8 @@
 int main() {
    // printf() displays the string inside quotation
    printf("Hello, World!");
-   printf("Hello Mir")
+   printf("Hello Mir");
+   printf("Hello Kausik");
    return 0;
 }
 
